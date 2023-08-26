d3e117895d0d818b3dff4a9343472bf87a5cfdc9
jdmanzur <jade.almeida@dcomp.sor.ufscar.br>
2023-08-26 16:38:56 -0300

    adicionando rawShaderMaterial

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //console.log("oi")
 
+
 const scene = new THREE.Scene();
 
 const camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 , 1000); //FOV, Aspect Radio, dist mínima para ver e máxima
@@ -24,10 +25,49 @@ renderer.render( scene , camera1 );
 const geometry = new THREE.TorusGeometry(10,3,16,100)
 const material = new THREE.MeshStandardMaterial({color:"yellow",})
 
-const torus = new THREE.Mesh(geometry, material);
+const customMaterial = new THREE.RawShaderMaterial({
+  
+  vertexShader: `
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+
+    attribute vec3 position;
+
+    void main(){
+      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    }
+  `,
+
+  fragmentShader: /*glsl*/`
+    //void main(){
+      //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //}
+
+    void main() {
+        //gl_FragColor = texture2D(texture1, vUv); // Displays Nothing
+        gl_FragColor = vec4(0.5, 0.2, 1.0, 1.0); // Works; Displays Flat Color
+    }
+
+  `,
+  wireframe: true,
+
+
+})
+
+
+const torus = new THREE.Mesh(geometry, customMaterial);
 
 
 scene.add(torus)
+
+
+
+
+
+
+
+
 
 
 
@@ -67,8 +107,7 @@ const loader = new GLTFLoader();
 loader.load('models/cat/scene.gltf',
   function (gltf) {
     model = gltf.scene;
-    scene.add(model);
-
+    scene.add(model)
   },
   undefined,
   function (error) {
@@ -76,6 +115,13 @@ loader.load('models/cat/scene.gltf',
   }
 );
 
+
+
+//model.traverse( ( object ) => {
+//  if ( object.isMesh ) object.material = customMaterial;
+//} );
+
+//scene.add(model);
 
 
 function animate() {
